5f95fd727d041b9750cd79388c1460a6b4d33530
William Siauw <36526721+Volcore001@users.noreply.github.com>
2020-08-15 19:50:41 -0700

    Added PseudoCompiler?

--- Handler.java
+++ Handler.java
@@ -2,6 +2,10 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import java.net.URI;
 
+import javax.tools.SimpleJavaFileObject;
+import java.net.URI;
+import java.io.IOException;
+
 // import com.google.gson.Gson;
 // import com.google.gson.GsonBuilder;
 import java.util.Map;
@@ -13,22 +17,37 @@ public class Handler implements RequestHandler<Map<String, String>, String> {
     public String handleRequest(Map<String, String> event, Context context) {
         StringBuffer sourceCode = new StringBuffer();
         sourceCode.append("public class HelloClass {\n");
-        sourceCode.append("   public String hello() { return \"hello\"; }");
+        sourceCode.append("  public static void main(String args[]) {");
+        sourceCode.append("    System.out.println(\"This is in another java file\");");
+        //sourceCode.append("  }");
         sourceCode.append("}");
+        InMemoryJavaCompiler imjc = new InMemoryJavaCompiler();
+        try {
+            Class<?> helloClass = imjc.compile("HelloClass", sourceCode.toString());   
+                return "okok";
 
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return "nope lol";
+        }
+    }
+
+    public static void main(String[] args) {
+        StringBuffer sourceCode = new StringBuffer();
+        sourceCode.append("public class HelloClass {\n");
+        sourceCode.append("  public static void main(String args[]) {");
+        sourceCode.append("    System.out.println(\"This is in another java file\");");
+        sourceCode.append("  }");
+        sourceCode.append("}");
         InMemoryJavaCompiler imjc = new InMemoryJavaCompiler();
         try {
             Class<?> helloClass = imjc.compile("HelloClass", sourceCode.toString());
-            
         } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        
-        return "ok";
     }
-
-    
 
 }
 
